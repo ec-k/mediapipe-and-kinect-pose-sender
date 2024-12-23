cff287236ab1866d1e7db2311e016d0e7c5767ab
EC-K <elxone.19jt@gmail.com>
2024-12-23 15:46:46 +0900

    feat: add throttle in main loop

--- mediapipe_inference/src/main.py
+++ mediapipe_inference/src/main.py
@@ -1,10 +1,33 @@
 from holistic_pose_sender import HolisticPoseSender
 from holistic_detector import HolisticDetector
+from throttle import throttle
 import visualizer
 
 import cv2
 import mediapipe as mp
 import numpy as np
+
+import time
+
+def main_loop():
+  # Break in key Ctrl+C pressed
+  if cv2.waitKey(5) & 0xFF == 27:
+    return False
+
+  image = cv2.cvtColor(np_mmap, cv2.COLOR_BGRA2RGB)
+  holistic_detector.inference(image)
+  pose_sender.send_holistic_landmarks(holistic_detector.results())
+
+  # Visualize resulted landmarks
+  annotated_image = image
+  if holistic_detector.results().pose_landmarks is not None:
+    annotated_image = visualizer.draw_pose_landmarks_on_image(annotated_image, holistic_detector.results().pose_landmarks)
+  if holistic_detector.results().hand_landmarks is not None:
+    annotated_image = visualizer.draw_hand_landmarks_on_image(annotated_image, holistic_detector.results().hand_landmarks)
+  if holistic_detector.results().face_results is not None:
+    annotated_image = visualizer.draw_face_landmarks_on_image(annotated_image, holistic_detector.results().face_results)
+  cv2.imshow('MediaPipe Landmarks', cv2.flip(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR), 1))
+  return True
 
 
 if __name__ == "__main__":
@@ -27,22 +50,10 @@ if __name__ == "__main__":
       shape=(height, width, 4),
   )
 
-  while True:
-      # Break in key Ctrl+C pressed
-      if cv2.waitKey(5) & 0xFF == 27:
-          break
+  doLoop = True
 
-      image = cv2.cvtColor(np_mmap, cv2.COLOR_BGRA2RGB)
-      holistic_detector.inference(image)
-      pose_sender.send_holistic_landmarks(holistic_detector.results())
+  while doLoop:
+    doLoop = main_loop()
+    time.sleep(1/60)
 
-      # Visualize resulted landmarks
-      annotated_image = image
-      if holistic_detector.results().pose_landmarks is not None:
-        annotated_image = visualizer.draw_pose_landmarks_on_image(annotated_image, holistic_detector.results().pose_landmarks)
-      if holistic_detector.results().hand_landmarks is not None:
-        annotated_image = visualizer.draw_hand_landmarks_on_image(annotated_image, holistic_detector.results().hand_landmarks)
-      if holistic_detector.results().face_results is not None:
-        annotated_image = visualizer.draw_face_landmarks_on_image(annotated_image, holistic_detector.results().face_results)
-      cv2.imshow('MediaPipe Landmarks', cv2.flip(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR), 1))
   cv2.destroyAllWindows()
